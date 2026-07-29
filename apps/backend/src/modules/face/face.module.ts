61c7292bae9a37449b5c4140a7c5704e05c2019e
Aditya Bhure <aadityabhure03@gmail.com>
2026-07-29 20:37:40 +0530

    feat: add AuditModule import to FaceModule

--- apps/backend/src/modules/face/face.module.ts
+++ apps/backend/src/modules/face/face.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { FaceController } from './face.controller';
 import { FaceService } from './face.service';
 import { FaceRegistrationService } from './face-registration.service';
+import { AuditModule } from '../audit/audit.module';
 
 @Module({
+  imports: [AuditModule],
   controllers: [FaceController],
   providers: [FaceService, FaceRegistrationService],
   exports: [FaceService, FaceRegistrationService],
